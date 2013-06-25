b3e97e8bd8f45aa846a778793ce4ac568fb9ccea
naciass <nataliaamusial@gmail.com>
2013-06-25 16:32:36 +0200

    kll

--- 2010 c/zad1.cpp
+++ 2010 c/zad1.cpp
@@ -18,6 +18,29 @@ public:
         }
         void add(const char* tekst)
         {
+            El *wsk1;
+            wsk1=element;
+            while(wsk1->nastepny!=0)
+            {
+                if((wsk1->napis.c_str).compare(tekst.c_str)==0))
+                {
+                    wsk1->liczba++; return;
+                }
+
+                 wsk1=wsk1->nastepny;
+            }
+              if(wsk1->nastepny==0)
+                {
+
+
+                    wsk1->nastepny=new El;
+                    wsk1=wsk1->nastepny;
+                    wsk1->liczba=1;
+                    wsk1->napis=tekst;
+
+                }
+
+
 
         }
 
