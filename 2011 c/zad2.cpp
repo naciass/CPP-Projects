6c35d9799eedf4591dddec56b64d3c4a6bd8f7a5
swiezy <s0wiezy@gmail.com>
2013-06-25 13:34:00 +0200

    kompiluje sie..., tak sie to robi?

--- 2011 c/zad2.cpp
+++ 2011 c/zad2.cpp
@@ -2,26 +2,39 @@
 #include <iostream>
 
 using namespace std;
-
+enum Typ{INT, DOUBLE, STRING};
 class Obiekt{
-    virtual wartosc;
     public:
-        virtual get(){return wartosc;}
+        virtual Typ getType()=0;
+        virtual void *get()=0;
+        virtual void set(void * v);
 };
 
 class Int: public Obiekt
 {
     int wartosc;
+public:
+    Typ getType(){return INT;};
+    void *get(){return &wartosc;}
+    void set(void *v){wartosc=*reinterpret_cast<int*>(v);}
 };
 
 class Double: public Obiekt
 {
     double wartosc;
+public:
+    Typ getType(){return DOUBLE;}
+    void *get(){return &wartosc;}
+    void set(void *v){wartosc=*reinterpret_cast<double*>(v);}
 };
 
 class String: public Obiekt
 {
     string wartosc;
+public:
+    Typ getType(){return STRING;};
+    void *get(){return &wartosc;}
+    void set(void *v){wartosc=*reinterpret_cast<string*>(v);}
 };
 
 class Sequence
@@ -34,7 +47,12 @@ public:
         Obiekt *nowy;
         for(it=seq.begin(); it!=seq.end();it++)
         {
-            typeid
+            switch((*it)->getType())
+            {
+                case INT: nowy=new Int;nowy->set((*it)->get()); break;
+                case DOUBLE: nowy=new Double;nowy->set((*it)->get()); break;
+                case STRING: nowy=new String;nowy->set((*it)->get()); break;
+            }
         }
     }
 };
